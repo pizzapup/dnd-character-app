affd8460650d01bdfb9febffe5f5442c7421e498
pizzapup <57334884+pizzapup@users.noreply.github.com>
2023-08-06 22:04:35 -0400

    roll for scores

--- src/components/DiceMiniGame/DiceMiniGame.js
+++ src/components/DiceMiniGame/DiceMiniGame.js
@@ -1,20 +1,31 @@
 import React from "react";
 import "./DiceMiniGame.css";
 import {ManualRoll} from "./Roll/Roll";
+import {Typography} from "@mui/material";
+import {Container, Box} from "@mui/material";
 
 const DiceMiniGame = () => {
   return (
-    <div className="container">
-      <div className="form-header">
-        <h1 style={{textAlign: "center", margin: 5, padding: 5}}>
-          Roll for Scores
-        </h1>
-
-        <div style={{minHeight: "150px"}}>
-          <ManualRoll />
-        </div>
-      </div>
-    </div>
+    <Container>
+      <Typography
+        component="h1"
+        sx={{
+          fontSize: "1.5rem",
+          fontWeight: "bold",
+          textAlign: "center",
+        }}
+      >
+        Roll for Scores
+      </Typography>
+      <Typography variant="body1" sx={{py: 1}}>
+        Calculate your scores and modifiers from your dice rolls! Submit your
+        dice values manually or click the "roll" button to generate random
+        values.
+      </Typography>
+      <Box sx={{minHeight: "150px"}}>
+        <ManualRoll />
+      </Box>
+    </Container>
   );
 };
 
